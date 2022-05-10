784a3f1968f1bc001ef6d85c4195723220308737
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-05-10 13:05:29 -0500

    fix: make contracts inherit from ERC20

--- src/lesson_3/EtherWrapper.sol
+++ src/lesson_3/EtherWrapper.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-import {ERC20Mock} from "yield-utils-v2/contracts/mocks/ERC20Mock.sol";
+import {ERC20} from "yield-utils-v2/contracts/token/ERC20.sol";
 
 /**
 @title A wrapper for Ether
 @author Sabnock01
 @notice You can use this contract to wrap Ether
 */
-contract EtherWrapper is ERC20Mock {
+contract EtherWrapper is ERC20 {
     ///@notice Event emitted when tokens are wrapped
     event Wrapped(address indexed from, uint256 amount);
     ///@notice Event emitted when tokens are unwrapped
@@ -18,13 +18,13 @@ contract EtherWrapper is ERC20Mock {
     ///@param name the token's name
     ///@param symbol the token's symbol
     ///@dev the token will have 18 decimals as specified by the constructor in ERC20Mock
-    constructor(string memory name, string memory symbol) ERC20Mock(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol, 18) {}
 
     ///@notice Wraps Ether
     ///@dev emits Wrapped event when transfer successful and error message upon failure
-    receive() external payable {
+    function wrap() public payable {
         _mint(msg.sender, msg.value);
-        emit Wrapped(msg.sender, msg.value);  
+        emit Wrapped(msg.sender, msg.value);
     }
 
     ///@notice Unwraps Ether
@@ -35,6 +35,11 @@ contract EtherWrapper is ERC20Mock {
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Failed to unwrap tokens!");
         emit Unwrapped(msg.sender, amount);
+    }
+
+    ///@notice Calls Wrap function when contract is sent Ether 
+    receive() external payable {
+        wrap();
     }
 
 }
